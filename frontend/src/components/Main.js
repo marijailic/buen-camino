b2ca9bbaef26be8a6a38e97373da9a8fc89b9ccb
Marija Ilić <marija.cbsqg@gmail.com>
2025-06-28 20:38:18 +0200

    wip

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
+import { haversine } from "../helpers/distanceHelper";
 
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
@@ -12,13 +13,52 @@ L.Icon.Default.mergeOptions({
     shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
 });
 
+const redIcon = new L.Icon({
+    iconUrl:
+        "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png",
+    shadowSize: [41, 41],
+});
+
+const yellowIcon = new L.Icon({
+    iconUrl:
+        "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-yellow.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png",
+    shadowSize: [41, 41],
+});
+
+const blueIcon = new L.Icon({
+    iconUrl:
+        "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png",
+    shadowSize: [41, 41],
+});
+
 const Main = () => {
     const [position, setPosition] = useState(null);
+
+    let positions = [];
+    for (let i = 0; i < 50; i++) {
+        positions.push([
+            Math.random() + 45.1772416 - 0.5,
+            Math.random() + 13.9722752 - 0.5,
+        ]);
+    }
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(
             (pos) => {
                 const { latitude, longitude } = pos.coords;
+                console.log(latitude, longitude);
                 setPosition([latitude, longitude]);
             },
             (err) => {
@@ -39,9 +79,25 @@ const Main = () => {
                         attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Marker position={position}>
+                    <Marker position={position} icon={redIcon}>
                         <Popup>You are here</Popup>
                     </Marker>
+                    {positions.map((pos) => {
+                        const distance = haversine(
+                            pos[0],
+                            pos[1],
+                            position[0],
+                            position[1]
+                        );
+                        return (
+                            <Marker
+                                position={pos}
+                                icon={distance < 30 ? yellowIcon : blueIcon}
+                            >
+                                <Popup>You are not here</Popup>
+                            </Marker>
+                        );
+                    })}
                 </MapContainer>
             ) : (
                 <p className="text-white">Loading map...</p>
---
+++ frontend/src/helpers/distanceHelper.js
@@ -0,0 +1,20 @@
+export function haversine(lat1, lon1, lat2, lon2) {
+    const R = 6371.0; // Zemljin radijus u kilometrima
+
+    const toRadians = (deg) => deg * (Math.PI / 180);
+
+    const dLat = toRadians(lat2 - lat1);
+    const dLon = toRadians(lon2 - lon1);
+
+    const a =
+        Math.sin(dLat / 2) ** 2 +
+        Math.cos(toRadians(lat1)) *
+            Math.cos(toRadians(lat2)) *
+            Math.sin(dLon / 2) ** 2;
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return R * c; // udaljenost u km
+}
+
+// 45.1772416 13.9722752
