6961fa41550cb63ecd690cfed0fe5b24863dd005
Marija Ilić <marija.cbsqg@gmail.com>
2025-07-01 22:13:21 +0200

    wip

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-# Run Laravel migrations
-php artisan migrate --force
+~/bin/composer install --optimize-autoloader --no-dev --no-interaction --no-ansi
 
-# Seed the database
-php artisan db:seed --force
+php artisan config:clear
+php artisan cache:clear
+php artisan view:clear
+php artisan route:clear
+
+php artisan view:cache
+php artisan route:cache
+php artisan config:cache
+
+php artisan migrate --force
+php artisan queue:restart
+
+php artisan up
+
+if [ -d "vendor" ]; then
+  php artisan down
+  php artisan config:clear
+fi
