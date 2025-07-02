dba1b34139d88274b91003cb307bc93c302afbe0
Marija Ilić <marija.cbsqg@gmail.com>
2025-07-02 15:24:03 +0200

    d8

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,21 +2,13 @@
 
 echo "Installing Composer dependencies..."
 composer install --no-dev --optimize-autoloader
-composer require fakerphp/faker
 
-# Clear all caches
 php artisan optimize:clear
-
-# Rebuild all caches
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-# Show routes (optional)
 php artisan route:list
 
-echo "Running Laravel migrations..."
 php artisan migrate --force
-
-echo "Running Laravel seeders..."
 php artisan db:seed --force || true
