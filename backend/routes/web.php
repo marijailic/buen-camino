9e690c513efb03886b4cac1c5a35d008fecc3520
Marija Ilić <marija.cbsqg@gmail.com>
2025-07-02 14:20:02 +0200

    d2

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Foundation\Application;
 
 Route::get('/', function () {
-    return response()->json([
-        'laravel_version' => Application::VERSION,
-    ]);
+    return redirect('/up');
 });
