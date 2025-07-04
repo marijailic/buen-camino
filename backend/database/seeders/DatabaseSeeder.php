bf384156d41e7c70767a5c17e303893946d13ae5
Marija Ilić <marija.cbsqg@gmail.com>
2025-07-04 18:16:51 +0200

    fix

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -12,14 +12,6 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-
-        DB::table('messages')->truncate();
-        DB::table('posts')->truncate();
-        DB::table('users')->truncate();
-
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-
         User::factory()->create([
             'email' => 'example@example.com',
             'latitude' => 45.0950,    // Labin lat
