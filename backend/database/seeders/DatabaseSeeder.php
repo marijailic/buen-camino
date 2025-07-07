e7dff45997f87bbcf13d7fe840504ceeee942c0e
Marija Ilić <marija.cbsqg@gmail.com>
2025-07-07 20:29:51 +0200

    last?

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -10,9 +10,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create([
-            'email' => 'example@example.com',
-            'latitude' => 45.0892,    // Labin lat
-            'longitude' => 14.1197,   // Labin lon
+            'email' => 'testuser@test.com',
+            'latitude' => 44.9620,    // Vodnjan lat
+            'longitude' => 13.8547,   // Vodnjan lon
         ]);
     }
 }
